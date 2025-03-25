9cf325fe53333126223145ac3b03aac51b81cb2d
Jack Anderson <jack@alacriware.com>
2025-03-24 20:31:36 -0400

    update auth routes with error checking and json responses and binding

--- auth/auth.go
+++ auth/auth.go
@@ -1,64 +1,109 @@
 package auth
 
 import (
+	"crypto/rand"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
-	// "crypto"
+
+	"github.com/windingtheropes/budget/json"
+	b64 "encoding/base64"
+	"log"
 )
 
-type NewAccountForm struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
-type LoginForm struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
-type GenericResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-type LoginResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-	Token   string `json:"token"`
-}
-
+// Authentication routes
 func LoadRoutes(engine *gin.Engine) {
+	// New Account
 	engine.POST("/account/new", func(ctx *gin.Context) {
-		body := NewAccountForm{}
-		if err := ctx.ShouldBindJSON(&body); err != nil {
-			ctx.JSON(400, GenericResponse{
-				Code:    400,
-				Message: "Invalid JSON.",
-			})
-			ctx.Abort()
+		body := json.NewAccountForm{}
+		if err := ctx.BindJSON(&body); err != nil {
+			json.NewResponse(ctx, 400, "Invalid JSON.");
+			return
+		}
+		
+		users, err := GetUser(body.Email)
+		if err != nil {
+			log.Fatal(err)
+			json.NewResponse(ctx, 500, "Interal error.");
+			return
+		}
+		if len(users) != 0 {
+			json.NewResponse(ctx, 403, "Email already in use.");
 			return
 		}
 
-		// CHECK IF USER EXISTS
-		// ctx.JSON(403, GenericResponse{
-		// 	Code:    403,
-		// 	Message: "User Exists.",
-		// })
+		// Password not hashed
+		user_id, err := AddUser(body.Name, body.Email, body.Password);
+		if err != nil {
+			log.Fatal(err)
+			json.NewResponse(ctx, 500, "Interal error.");
+			return
+		}
 
-		ctx.JSON(200, GenericResponse{
-			Code:    200,
-			Message: "Created.",
-		})
+		json.NewResponse(ctx, 200, fmt.Sprintf("Created user %d", user_id));
 	})
-	engine.GET("/account/login", func(ctx *gin.Context) {
-		// CHECK CREDENTIALS
-		// ctx.JSON(403, GenericResponse{
-		// 	Code:    403,
-		// 	Message: "Invalid Credentials.",
-		// })
 
-		// IF CORRECT
-		// ctx.JSON(200, GenericResponse{
-		// 	Code:    200,
-		// 	Token: "NEWTOKENGEN",
-		// })
-		ctx.Status(403)
+	// Login gives a session token
+	engine.PUT("/account/login", func(ctx *gin.Context) {
+		body := json.LoginForm{}
+		// Bind the json to the loginform body, or return an error
+		if err := ctx.BindJSON(&body); err != nil {
+			json.NewResponse(ctx, 400, "Invalid JSON.");
+			return
+		}
+		
+		// catch unknown errors
+		users, err := GetUser(body.Email)
+		if err != nil {
+			log.Fatal(err)
+			json.NewResponse(ctx, 500, "Interal error.");
+			return
+		}
+		if len(users) == 1 {
+			usr := users[0];
+			if body.Password == usr.Password {
+				t, _, err := NewSession(usr.Id)
+				var token string = t[0]
+
+				if err != nil {
+					log.Fatal(err)
+					json.NewResponse(ctx, 500, "Interal error.");
+					return
+				}
+
+				ctx.AbortWithStatusJSON(200, json.SessionResponse{
+					Code: 200,
+					Token: token,
+				})
+				return
+			} else {
+				// password incorrect
+				json.NewResponse(ctx, 403, "Invalid credentials.");
+				return
+			}
+		} else if len(users) == 0 {
+			// user doesn't exist with email
+			json.NewResponse(ctx, 403, "Invalid credentials.");
+			return
+		}
 	})
+}
+
+// Returns whether the token belongs to a session, and if so, the user id which it belongs to.
+func IsSession(token string) (bool, int) {
+	sessions, err := GetSession(token);
+	if err != nil {
+		return false, 0
+	}
+	if len(sessions) == 0 {
+		return false, 0
+	}
+	
+	session := sessions[0];
+	return true, session.User_Id
+}
+func GenToken(length int) string {
+	var keyBytes = make([]byte, length);
+	rand.Read(keyBytes);
+	return b64.RawStdEncoding.EncodeToString(keyBytes);
 }
