66009f47512b2f373e743b06bee11921c218f785
Jack Anderson <jack@alacriware.com>
2025-03-25 21:06:29 -0400

    load argent routes

--- server.go
+++ server.go
@@ -2,18 +2,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/windingtheropes/budget/auth"
+	"github.com/windingtheropes/budget/argent"
 	"github.com/windingtheropes/budget/dotenv"
 	"github.com/windingtheropes/budget/based"
-	// "crypto"
 )
 
 func main() {
 	dotenv.Init()
 	based.InitDB()
-
+	
 	// db := based.DB();
 	engine := gin.Default();
 
 	auth.LoadRoutes(engine);
+	argent.LoadRoutes(engine);
+	
 	engine.Run("localhost:3000");
 }
