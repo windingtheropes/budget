bc7b293a901254e6b8146128ea0f62bfc02d81f5
jack <jaandersonck@gmail.com>
2025-05-14 14:38:23 -0400

    add hydration for tags with tagbudgets, and transactions with budgetentries

--- argent/hydra.go
+++ argent/hydra.go
@@ -10,8 +10,13 @@ func HydrateTransactionsWithTags(transactions []types.TransactionEntry) ([]types
 		d_trans := transactions[i]
 		tags, err := GetTransactionTags(d_trans.Id)
 		if err != nil {
-			return make([]types.HydTransactionEntry, 0), err
+			return nil, err
 		}
+		budget_entries, err := GetBudgetEntries(d_trans.Id)
+		if err != nil {
+			return nil, err
+		}
+
 		hydratedTransactions = append(hydratedTransactions, types.HydTransactionEntry{
 			Id:             d_trans.Id,
 			User_Id:        d_trans.User_Id,
@@ -22,9 +27,27 @@ func HydrateTransactionsWithTags(transactions []types.TransactionEntry) ([]types
 			Tags:           tags,
 			Unix_Timestamp: d_trans.Unix_Timestamp,
 			Vendor:         d_trans.Vendor,
+			Budget_Entries: budget_entries,
 		})
 	}
 	return hydratedTransactions, nil
+}
+
+func HydrateTagsWithTagBudgets(tags []types.Tag) ([]types.HydTag, error) {
+	var hydratedTags []types.HydTag
+	for i := range tags {
+		tag := tags[i]
+		tagBudgets, err := GetTagBudget(tag.Id)
+		if err != nil {
+			return nil, err
+		}
+		hydratedTags = append(hydratedTags, types.HydTag{
+			Id:      tag.Id,
+			Name:    tag.Name,
+			Tag_Budgets: tagBudgets,
+		})
+	}
+	return hydratedTags, nil
 }
 
 func AddTagsById(transaction_id int64, tag_ids []int) error {
