daeba368592665ee4c09e14b2aafb1024b6b588f
jack <jaandersonck@gmail.com>
2025-06-11 16:43:27 -0400

    move scattered table deffinitions to tables.go package for more idiomatic access

--- argent/tag.go
+++ argent/tag.go
@@ -3,13 +3,9 @@ package argent
 import (
 	"log"
 
-	"github.com/windingtheropes/budget/based"
+	"github.com/windingtheropes/budget/tables"
 	"github.com/windingtheropes/budget/types"
 )
-
-var TagTable = based.NewTable[types.Tag, types.TagForm]("tag")
-var TagOwnershipTable = based.NewTable[types.TagOwnership, types.TagOwnershipForm]("tag_ownership")
-var TagAssignmentTable = based.NewTable[types.TagAssignment, types.TagAssignmentForm]("tag_assignment")
 
 func UserTagNameExists(tag_name string, user_id int64) bool {
 	tags, err := GetUserTags(user_id)
@@ -31,12 +27,12 @@ func UserTagNameExists(tag_name string, user_id int64) bool {
 
 func GetTransactionTags(entry_id int64) ([]types.Tag, error) {
 	var tags []types.Tag
-	assignments, err := TagAssignmentTable.Get("entry_id = ?", entry_id)
+	assignments, err := tables.TagAssignment.Get("entry_id = ?", entry_id)
 	if err != nil {
 		return nil, err
 	}
 	for i := 0; i < len(assignments); i++ {
-		res_tags, err := TagTable.Get("(id=?)",assignments[i].Tag_Id)
+		res_tags, err := tables.Tag.Get("(id=?)",assignments[i].Tag_Id)
 		if err != nil {
 			return nil, err
 		}
@@ -49,13 +45,13 @@ func GetTransactionTags(entry_id int64) ([]types.Tag, error) {
 
 func GetUserTags(user_id int64) ([]types.Tag, error) {
 	var userTags []types.Tag
-	ownership_records, err := TagOwnershipTable.Get("user_id=?", user_id)
+	ownership_records, err := tables.TagOwnership.Get("user_id=?", user_id)
 	if err != nil {
 		return nil, err
 	}
 	for i := 0; i < len(ownership_records); i++ {
 		record := ownership_records[i]
-		res_tags, err := TagTable.Get("(id=?)",record.Tag_Id)
+		res_tags, err := tables.Tag.Get("(id=?)",record.Tag_Id)
 		if err != nil {
 			return nil, err
 		}
@@ -66,17 +62,17 @@ func GetUserTags(user_id int64) ([]types.Tag, error) {
 	return userTags, nil
 }
 func NewUserTag(name string, user_id int64) (int64, error) {
-	tag_id, err := TagTable.New(types.TagForm{Name: name})
+	tag_id, err := tables.Tag.New(types.TagForm{Name: name})
 	if err != nil {
 		return 0, err
 	}
-	if _, err := TagOwnershipTable.New(types.TagOwnershipForm{User_Id: user_id, Tag_Id: int64(tag_id)}); err != nil {
+	if _, err := tables.TagOwnership.New(types.TagOwnershipForm{User_Id: user_id, Tag_Id: int64(tag_id)}); err != nil {
 		return 0, err
 	}
 	return tag_id, nil
 }
 func TagExistsOnEntry(tag_id int64, entry_id int64) bool {
-	tags, err := TagAssignmentTable.Get("entry_id = ?", entry_id)
+	tags, err := tables.TagAssignment.Get("entry_id = ?", entry_id)
 	if err != nil {
 		log.Fatal(err)
 		return false
@@ -94,7 +90,7 @@ func TagExistsOnEntry(tag_id int64, entry_id int64) bool {
 }
 
 func UserOwnsTag(user_id int64, tag_id int64) bool {
-	ownerships, err := TagOwnershipTable.Get("user_id=?",user_id)
+	ownerships, err := tables.TagOwnership.Get("user_id=?",user_id)
 	if err != nil {
 		return false
 	}
@@ -111,7 +107,7 @@ func UserOwnsTag(user_id int64, tag_id int64) bool {
 }
 
 func TagExists(tag_id int64) bool {
-	tags, err := TagTable.Get("(id=?)",tag_id)
+	tags, err := tables.Tag.Get("(id=?)",tag_id)
 	if err != nil {
 		return false
 	}
--- auth/auth.go
+++ auth/auth.go
@@ -3,13 +3,7 @@ package auth
 import (
 	"crypto/rand"
 	b64 "encoding/base64"
-
-	"github.com/windingtheropes/budget/based"
-	"github.com/windingtheropes/budget/types"
 )
-
-var UserTable = based.NewTable[types.User, types.UserForm]("usr")
-var SessionTable = based.NewTable[types.Session, types.SessionForm]("usr")
 
 func GenToken(length int) string {
 	var keyBytes = make([]byte, length)
